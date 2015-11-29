7842b0dcf0f0add9004033061b8f1e65259cb720
FormalRiceFarmer <mathiasgirouard@gmail.com>
2015-11-29 10:56:45 -0600

    fiddled with configs

--- moindustry_compat_mod_0.0.1/control.lua
+++ moindustry_compat_mod_0.0.1/control.lua
@@ -10,3 +10,10 @@
 --remote.call("MoIndustry","RegisterOre","coal","coal",0,1,1) -- None Infinite resource
 --remote.call("MoIndustry","RegisterOre","coal","coal",150,250,1) -- Infinite resource
 
+
+remote.call("MoIndustry","RegisterOre","y-res1","y-res1",2,500,1)
+remote.call("MoIndustry","RegisterOre","y-res2","y-res2",2,500,1)
+remote.call("MoIndustry","RegisterOre","iron-ore","iron-ore",1,1000,1)
+remote.call("MoIndustry","RegisterOre","copper-ore","copper-ore",2,500,1)
+remote.call("MoIndustry","RegisterOre","coal","coal",2,500,1)
+remote.call("MoIndustry","RegisterOre","stone","stone",2,500,1)
