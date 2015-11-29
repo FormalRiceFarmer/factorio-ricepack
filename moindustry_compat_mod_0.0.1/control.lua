f3bf69cfb716418c12a3d8bff76d9700aad75517
Ludsoe <kingludsoe@gmail.com>
2015-11-29 09:23:54 -0700

    Added a control mod for moindustry supported ores

---
+++ moindustry_compat_mod_0.0.1/control.lua
@@ -0,0 +1,12 @@
+
+
+
+
+----Lets start calling some stuff in.....
+--remote.call("MoIndustry","RegisterOre","ore-entity-name","result-item-name",minimum-amount,normal-amount,mining-yield)
+--												string			string			 number		     number		  number
+
+--Example
+--remote.call("MoIndustry","RegisterOre","coal","coal",0,1,1) -- None Infinite resource
+--remote.call("MoIndustry","RegisterOre","coal","coal",150,250,1) -- Infinite resource
+
